d0680b448307e34e6d076e0aa0f78c96dd8b7a94
j0n0curry <58121289+j0n0curry@users.noreply.github.com>
2022-03-08 14:40:36 +0000

    Update ePCR_new_cal_OX_viewv1.py

--- ePCR_new_cal_OX_viewv1.py
+++ ePCR_new_cal_OX_viewv1.py
@@ -117,15 +117,15 @@ def main():
             return('No_Call')
         elif row['norm_N_Cov'] < 0.5 and row['norm_RNaseP'] > 0.2:
             return('Negative Patient')
-        elif row['norm_N_Cov'] > 0.5 and row['norm_N_Cov'] <= 1.0 and row['norm_RNaseP'] >0.2:
+        elif row['norm_N_Cov'] > 0.5 and row['norm_N_Cov'] <= 1.0 and row['norm_RNaseP'] >0.4:
             return('PLOD')
-        elif row['norm_N_Cov'] > 1 and row['norm_N_Cov'] <= 1.5 and row['norm_RNaseP'] >= 0.2:
+        elif row['norm_N_Cov'] > 1 and row['norm_N_Cov'] <= 1.5 and row['norm_RNaseP'] > 0.4:
             return('N_Cov Paitent Positive')
         elif row['norm_N_Cov'] > 1.5 and row['norm_RNaseP'] >= 0.2:
             return('N_Cov Paitent Positive plus E')
-        elif row['norm_N_Cov'] > 1 and row['norm_N_Cov'] <= 1.5 and row['norm_RNaseP']<=0.4:
+        elif row['norm_N_Cov'] > 1 and row['norm_N_Cov'] <= 1.5 and row['norm_RNaseP']<0.4:
             return('Control_N_Cov')
-        elif row['norm_N_Cov'] > 1.5 and row['norm_RNaseP']<= 0.4:
+        elif row['norm_N_Cov'] > 1.5 and row['norm_RNaseP']<0.4:
             return('Control_N_Cov_plus_E')
         elif row['norm_N_Cov'] > 0.5 and row['norm_N_Cov'] <= 1.0 and row['norm_RNaseP'] <0.3:
             return'CTRL_PLOD'
